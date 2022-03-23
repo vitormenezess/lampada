4d54f644dfcc871cb31c505a84410361bbc8d4c7
vitormenezess <vitormenezes43@icloud.com>
2022-03-23 13:13:10 -0300

    finalizado

--- lampada.js
+++ lampada.js
@@ -1,17 +1,32 @@
-const ligado = document.getElementById("ligado");
-const desligado = document.getElementById("desligado");
+const ligaDesliga = document.getElementById("ligaDesliga");
 const lamp = document.getElementById("lamp");
 
 function lampOn() {
-  lamp.src = "imagens/ligada.jpg";
+  if (!isLampQ()) lamp.src = "imagens/ligada.jpg";
 }
 function lampOff() {
-  lamp.src = "imagens/desligada.jpg";
+  if (!isLampQ()) lamp.src = "imagens/desligada.jpg";
 }
 function lampQ() {
   lamp.src = "imagens/quebrada.jpg";
 }
+function isLampQ() {
+  return lamp.src.indexOf("quebrada") > -1;
+}
+function lampOnOff() {
+  if (ligaDesliga.textContent == "Ligar") {
+    lampOn();
+    ligaDesliga.textContent = "Deligar";
+    ligaDesliga.style.background = 'red'
+  } else {
+    lampOff();
+    ligaDesliga.textContent = "Ligar";
+    ligaDesliga.style.background = 'rgb(4, 207, 4)'
+  }
+}
 
-ligado.addEventListener("click", lampOn);
-desligado.addEventListener("click", lampOff);
+ligaDesliga.addEventListener("click", lampOnOff);
+
 lamp.addEventListener("click", lampQ);
+lamp.addEventListener("mouseover", lampOn);
+lamp.addEventListener("mouseleave", lampOff);
